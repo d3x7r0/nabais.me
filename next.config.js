80932d95ea65b2d743f7facf66d57d5788dc76f0
Luis Nabais <dextro@gmail.com>
2020-12-03 18:32:03 +0000

    Bump to next.js v10

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /* eslint-env node */
 /* eslint-disable import/order */
 const { resolve } = require('path')
-const withPrefresh = require('@prefresh/next')
+const withPreact = require('next-plugin-preact')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
@@ -17,12 +17,11 @@ const nextConfig = {
   trailingSlash: true,
   experimental: {
     modern: true,
-    polyfillsOptimization: true,
   },
   cssLoaderOptions: {
     url: false,
   },
-  webpack(config, { dev, isServer }) {
+  webpack(config) {
     TRANSPILED_MODULES.forEach((moduleName) => {
       const pkg = require(resolve(__dirname, '.', 'node_modules', moduleName, 'package.json'))
 
@@ -38,43 +37,12 @@ const nextConfig = {
       },
     )
 
-    const splitChunks = config.optimization && config.optimization.splitChunks
-    if (splitChunks) {
-      const cacheGroups = splitChunks.cacheGroups
-      const preactModules = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/
-      if (cacheGroups.framework) {
-        cacheGroups.preact = Object.assign({}, cacheGroups.framework, {
-          test: preactModules,
-        })
-        cacheGroups.commons.name = 'framework'
-      } else {
-        cacheGroups.preact = {
-          name: 'commons',
-          chunks: 'all',
-          test: preactModules,
-        }
-      }
-    }
-
-    // Install webpack aliases:
-    const aliases = config.resolve.alias || (config.resolve.alias = {})
-    aliases.react = aliases['react-dom'] = 'preact/compat'
-
-    // inject Preact DevTools
-    if (dev && !isServer) {
-      const entry = config.entry
-      config.entry = () => entry().then(entries => {
-        entries['main.js'] = ['preact/debug'].concat(entries['main.js'] || [])
-        return entries
-      })
-    }
-
     return config
   },
 }
 
 module.exports = withTM(
   withBundleAnalyzer(
-    withPrefresh(nextConfig),
-  ),
+    withPreact(nextConfig),
+  )
 )
