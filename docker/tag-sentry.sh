09beed8365089a432dc2c60009abf2ae5314092d
Luis Nabais <dextro@gmail.com>
2020-12-25 12:02:04 +0000

    Add missing command to sentry release script

--- docker/tag-sentry.sh
+++ docker/tag-sentry.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
 export SENTRY_ORG=nonsensebb
-
-sentry-cli info
+export SENTRY_PROJECT=nabais_me
 
 VERSION=$(sentry-cli releases propose-version)
 
-sentry-cli releases new -p nabais_me "$VERSION"
+sentry-cli info
+
+echo "Creating new release"
+sentry-cli releases new -p "$SENTRY_PROJECT" "$VERSION"
+
+echo "Setting release commits"
 sentry-cli releases set-commits --auto "$VERSION"
+
+echo "Tagging release as deployed"
+sentry-cli releases deploys "$VERSION" new -e production
