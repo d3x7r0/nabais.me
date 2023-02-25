8afdd12bdab45d1e069d00e5487e1fcf98cd0acd
Luis Nabais <dextro@gmail.com>
2023-02-25 10:24:42 +0000

    Abort build script on failure

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,16 +4,16 @@ set -a; source .env; set +a
 
 VERSION=$(node_modules/.bin/sentry-cli releases propose-version)
 
-node_modules/.bin/sentry-cli info
+node_modules/.bin/sentry-cli info || exit -1
 
 echo "Building"
-npm run build
+npm run build || exit -1
 
 echo "Creating new release [$VERSION]"
-node_modules/.bin/sentry-cli releases new -p "$SENTRY_PROJECT" "$VERSION"
+node_modules/.bin/sentry-cli releases new -p "$SENTRY_PROJECT" "$VERSION" || exit -1
 
 echo "Setting release commits [$VERSION]"
-node_modules/.bin/sentry-cli releases set-commits --auto "$VERSION"
+node_modules/.bin/sentry-cli releases set-commits --auto "$VERSION" || exit -1
 
 echo "Tagging release as deployed [$VERSION]"
-node_modules/.bin/sentry-cli releases deploys "$VERSION" new -e production
+node_modules/.bin/sentry-cli releases deploys "$VERSION" new -e production || exit -1
