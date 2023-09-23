143c1a302de060a7c28ad1d3594bd83e07e68996
Luis Nabais <dextro@gmail.com>
2023-09-23 08:46:54 +0100

    Fix typo

--- src/js/hooks/storage.d.ts
+++ src/js/hooks/storage.d.ts
@@ -1,3 +1,3 @@
-export function useLocalStorage<T>(key: string, defaultValue: ? T): [T | undefined, (value: T) => T | undefined]
+export function useLocalStorage<T>(key: string, defaultValue?:  T): [T | undefined, (value: T) => T | undefined]
 
-export function useSessionStorage<T>(key: string, defaultValue: ? T): [T | undefined, (value: T) => T | undefined]
+export function useSessionStorage<T>(key: string, defaultValue?:  T): [T | undefined, (value: T) => T | undefined]
